746f8832417b68b736e433fd310a5783123633c1
Sam A. Horvath-Hunt <hello@samhh.com>
2021-07-26 22:24:43 +0100

    Add lifted semigroup (<>^) util

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -19,3 +19,9 @@ concatWhereBy f p (x:y:zs)
   | p x y     =     concatWhereBy f p (f x y : zs)
   | otherwise = x : concatWhereBy f p (y     : zs)
 
+-- Borrowed from: https://hackage.haskell.org/package/intro-0.9.0.0/docs/Intro.html#v:-60--62--94-
+-- | Semigroup concat lifted to an applicative context.
+(<>^) :: (Applicative f, Semigroup a) => f a -> f a -> f a
+(<>^) = liftA2 (<>)
+infixr 6 <>^
+{-# INLINE (<>^) #-}
