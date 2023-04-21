b51e18b88ace8d915ee5a5ce0fd6f422d827ae3f
Sam A. Horvath-Hunt <hello@samhh.com>
2023-04-21 18:52:50 +0100

    Add Nix shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ghc
+            haskell-language-server
+            cabal-install
+            hlint
+          ];
+        };
+      });
+}
